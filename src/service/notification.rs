8c1dc12c1307d5a8394709afd0880b8a399d2f31
wuyu0107 <mkim050107@gmail.com>
2025-03-27 14:49:10 +0700

    Implement receive_notification function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -90,4 +90,9 @@ impl NotificationService {
             ))
         }
     }
+
+    pub fn receive_notification(payload: Notification) -> Result<Notification> {
+        let subscriber_result: Notification = NotificationRepository::add(payload);
+        return Ok(subscriber_result);
+    }
 }
