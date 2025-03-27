2bb6cf2978ded2603fe6f2b2614d5038a41da98b
wuyu0107 <mkim050107@gmail.com>
2025-03-27 14:45:27 +0700

    Implement unsubscribe function in Notification service.

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -18,7 +18,7 @@ impl NotificationService {
         let product_type_clone = String::from(product_type);
         return thread::spawn(move || Self::subscribe_request(product_type_clone))
             .join().unwrap();
-    }   
+    } 
 
     #[tokio::main]
     async fn subscribe_request(product_type: String) -> Result<SubscriberRequest> {
@@ -46,6 +46,42 @@ impl NotificationService {
                 Err(y) => Err(compose_error_response(
                     Status::NotAcceptable, 
                     y.to_string()
+                ))
+            },
+            Err(e) => Err(compose_error_response(
+                Status::NotFound, 
+                e.to_string()
+            ))
+        }
+    }
+
+    pub fn unsubscribe(product_type: &str) -> Result<SubscriberRequest> {
+        let product_type_clone = String::from(product_type);
+        return thread::spawn(move || Self::unsubscribe_request(product_type_clone))
+            .join().unwrap();
+    }
+
+    #[tokio::main]
+    async fn unsubscribe_request(product_type: String) -> Result<SubscriberRequest> {
+        let product_type_upper: String = product_type.to_uppercase();
+        let product_type_str: &str = product_type_upper.as_str();
+        let notification_receiver_url: String = format!("{}/receiver",
+            APP_CONFIG.get_instance_root_url());
+        let request_url: String = format!("{}/notification/unsubscribe/{}?url={}",
+            APP_CONFIG.get_publisher_root_url(), product_type_str, notification_receiver_url);
+        let request = REQWEST_CLIENT
+            .post(request_url.clone())
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .send().await;
+        log::warn_!("Sent unsubscribe request to: {}", request_url);
+
+        return match request {
+            Ok(f) => match f.json::<SubscriberRequest>().await {
+                Ok(x) => Ok(x),
+                Err(_y) => Err(compose_error_response(
+                    Status::NotFound, 
+                    String::from("Already unsubscribed to the topic.")
                 ))
             },
             Err(e) => Err(compose_error_response(
